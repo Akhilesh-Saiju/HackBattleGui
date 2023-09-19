0e6e64f36669efd491101b940ff32418e4f41037
akhilesh <akhilesh.saiju2022@vitstudent.ac.in>
2023-09-20 04:28:01 +0530

    Commit 3

---
+++ index.js
@@ -0,0 +1,13 @@
+ var patientCSV = "";
+ var arrObj = [];
+ var lines = patientCSV.split('\n');
+ var headers = lines[0].split(',');
+
+ for(var i = 0; i<lines.length; i++){
+    varData = lines[i].split(',');
+    arrObj[i] = {};
+    for(var j =0; j<rowData.lenght; j++){
+        arrObj[i][headers[j]]=rowData[j];
+    }
+    console.
+ }
